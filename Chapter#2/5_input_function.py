815561ebcfa6fccd07265b4544f59eb530217cca
NOOR UL NISA <bc220401664noo@vu.edu.pk>
2024-09-15 23:58:55 +0500

    input function

---
+++ Chapter#2/5_input_function.py
@@ -0,0 +1,9 @@
+# input function:
+
+# input function is used to take input from the user.
+
+a = input("Enter your name: ")
+b = input("Enter your age: ")
+c = input("Enter your city: ")
+
+print(" My name is " + a + " \n my age is " + b + " \nI live in " + c)
