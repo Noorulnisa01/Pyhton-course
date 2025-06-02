0d70f97197448e3cdec8ffca2de6abdfd6475f0f
NOOR UL NISA <bc220401664noo@vu.edu.pk>
2025-06-02 13:37:58 +0500

    list and tuple

---
+++ Chapter#4_List_and_Tuples/code/Tuple.py
@@ -0,0 +1,6 @@
+                # --------------------- Tuple ---------------------
+
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Tuples are created using round brackets ().
+
+# Example of a tuple
